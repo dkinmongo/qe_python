6968a69d55f2f966c58ac823e328a0b2c1cc7831
dk.kim <81962048+dkinmongo@users.noreply.github.com>
2024-08-12 19:57:53 +0900

    Add files via upload

--- make_data_key.py
+++ make_data_key.py
@@ -68,3 +68,54 @@ data_key_id_4 = client_encryption.create_data_key(
 )
 print(f"Key with ID {data_key_id_4} and keyAltName 'dataKey4' has been created.")
 
+
+# start-create-enc-collection
+encrypted_db_name = "test"
+encrypted_coll_name = "patients"
+encrypted_fields_map = {
+    f"{encrypted_db_name}.{encrypted_coll_name}": {
+        "fields": [
+            {
+                "keyId": data_key_id_1,
+                "path": "patientId",
+                "bsonType": "int",
+                "queries": {"queryType": "equality"},
+            },
+            {
+                "keyId": data_key_id_2,
+                "path": "medications",
+                "bsonType": "array",
+            },
+            {
+                "keyId": data_key_id_3,
+                "path": "patientRecord.ssn",
+                "bsonType": "string",
+                "queries": {"queryType": "equality"},
+            },
+            {
+                "keyId": data_key_id_4,
+                "path": "patientRecord.billing",
+                "bsonType": "object",
+            },
+        ],
+    },
+}
+
+key_vault_namespace = "qe.__keyVault"
+
+auto_encryption = AutoEncryptionOpts(
+    kms_providers,
+    key_vault_namespace,
+    encrypted_fields_map=encrypted_fields_map,
+    crypt_shared_lib_path=credentials["SHARED_LIB_PATH"],
+)
+
+secure_client = MongoClient(connection_string, auto_encryption_opts=auto_encryption)
+# Drop the encrypted collection in case you created this collection
+# in a previous run of this application.
+secure_client.drop_database(encrypted_db_name)
+encrypted_db = secure_client[encrypted_db_name]
+encrypted_db.create_collection(encrypted_coll_name)
+print("Created encrypted collection!")
+# end-create-enc-collection
+
